32c8df32e1ea5db2e7e6a9b13c31bbdee9e1a5f1
attilaszasz <szasz.attila@gmail.com>
2014-04-18 15:13:22 +0300

    routing kinda works

--- dev-list.js
+++ dev-list.js
@@ -7,7 +7,9 @@ var DevList = {
     Collections: {},
     Models: {},
     Views: {},
-    Data: {},
+    Data: {
+        Editions: []
+    },
     Settings: {},
     Templates: {}
 };
@@ -15,64 +17,24 @@ var DevList = {
 DevList.App._Router = Backbone.Router.extend({
     routes: {
         "read/:year/:edition": "read",
-        "*default": "home"
     },
     read: function(year, edition) {
         console.log('year: ' + year + ', edition: ' + edition);
-        if (DevList.Models.CurrentEdition) {
-            DevList.Models.CurrentEdition.clear().initialize({ url: '/json/' + year + '_' + edition + '.json', title: 'Test title' });
-        } else {
-            DevList.Models.CurrentEdition = new DevList.App._Models.Edition({ url: '/json/' + year + '_' + edition + '.json', title: 'Test title' });
-        }
+        var ed = _.find(DevList.Data.Editions, function (e) {
+                return e.year == year && e.index == edition;
+            });
+
+        DevList.Views.MainMenu.setCurrentEdition(ed.url, ed.title);
         DevList.Models.CurrentEdition.fetch();
     },
-    home: function () {
-        console.log('home');
-        DevList.Models.CurrentEdition = new DevList.App._Models.Edition({ url: '/json/2014_16.json', title: 'Test title' });
-        DevList.Models.CurrentEdition.fetch();
-        DevList.Router.navigate('read/2014/16', { trigger: false });
-    }
-});
-
-DevList.App._Models.Years = Backbone.Model.extend({
-    url: "/json/index.json"
-});
-
-DevList.Models.Years = new DevList.App._Models.Years();
-
-DevList.App._Views.MainMenu = Backbone.View.extend({
-    initialize: function () {
-        _.bindAll(this, 'render');
-        this.model.bind("change reset", this.render);
-    },
-    render: function () {
-        this.$el.html(DevList.Templates.MainMenu.render({ years: this.model.toJSON() }));
-        return this;
-    },
-    events: {
-        'click a.main-menu-link': 'mainMenuClick'
-    },
-    mainMenuClick: function (event) {
-        var fileName = $(event.target).data('file-name');
-        var fileNameNoExtension = fileName.split('.')[0];
-        var year = fileNameNoExtension.split('_')[0];
-        var edition = fileNameNoExtension.split('_')[1];
-        DevList.Models.CurrentEdition.clear().initialize({ url: '/json/' + year + '_' + edition + '.json', title: 'Test title' });
-        // DevList.Models.CurrentEdition.instanceUrl = '/json/' + year + '_' + edition + '.json';
-        DevList.Models.CurrentEdition.fetch();
-        DevList.Router.navigate('read/' + year + '/' + edition, { trigger: false });
-    }
 });
 
 DevList.App._Models.Edition = Backbone.Model.extend({
-    url: function () {
-        return this.instanceUrl;
-    },
     initialize: function (props) {
-        this.instanceUrl = props.url;
+        this.url = props.url;
         this.title = props.title;
     },
-    title: function() {
+    title: function () {
         return this.title;
     },
 });
@@ -88,16 +50,75 @@ DevList.App._Views.Edition = Backbone.View.extend({
     },
 });
 
+DevList.App._Models.Years = Backbone.Model.extend({
+    url: "/json/index.json",
+});
+
+DevList.App._Views.MainMenu = Backbone.View.extend({
+    initialize: function () {
+        _.bindAll(this, 'render');
+        _.bindAll(this, 'menuLoaded');
+        _.bindAll(this, 'setCurrentEdition');
+        this.model.bind("change", this.menuLoaded);
+    },
+    render: function () {
+        this.$el.html(DevList.Templates.MainMenu.render({ years: this.model.toJSON() }));
+        return this;
+    },
+    menuLoaded: function (model, options) {
+        this.render();
+        console.log(model.changed);
+        _.each(model.changed.years, function(year, index, years) {
+            _.each(year.editions, function(edition, idx, list) {
+                DevList.Data.Editions.push({ title: edition.title, year: year.year, index: idx + 1, url:edition.url });
+            });
+        });
+        var edition;
+        if (Backbone.history.fragment) {
+            edition = _.find(DevList.Data.Editions, function (e) {
+                return e.year == Backbone.history.fragment.split('/')[1] && e.index == Backbone.history.fragment.split('/')[2];
+            });
+        } else {
+            edition = _.last(DevList.Data.Editions);
+        }
+        this.setCurrentEdition(edition.url, edition.title);
+        DevList.Views.Edition = new DevList.App._Views.Edition({ model: DevList.Models.CurrentEdition, el: '#edition' });
+        DevList.Models.CurrentEdition.fetch();
+        Backbone.history.start();
+        return this;
+    },
+    events: {
+        'click a.main-menu-link': 'mainMenuClick'
+    },
+    setCurrentEdition: function (fileName, title) {
+        console.log('SetCurrentEdition - fileName: ' + fileName + ', title: ' + title);
+        if (DevList.Models.CurrentEdition) {
+            DevList.Models.CurrentEdition.clear();
+            DevList.Models.CurrentEdition.url = '/json/' + fileName;
+            DevList.Models.CurrentEdition.set("title", title);
+        } else {
+            DevList.Models.CurrentEdition = new DevList.App._Models.Edition({ url: '/json/' + fileName, title: title });
+        }
+    },
+    mainMenuClick: function (event) {
+        var fileName = $(event.target).data('file-name');
+        var fileNameNoExtension = fileName.split('.')[0];
+        var year = fileNameNoExtension.split('_')[0];
+        var edition = fileNameNoExtension.split('_')[1];
+        this.setCurrentEdition(fileName, $(event.target).text());
+        DevList.Models.CurrentEdition.fetch();
+        DevList.Router.navigate('read/' + year + '/' + edition);
+        console.log(Backbone.history.fragment);
+    }
+});
 
 $(function () {
-    DevList.Router = new DevList.App._Router();
-    Backbone.history.start({ pushState: true, hashChange: true });
     DevList.Templates.MainMenu = Hogan.compile($('#main-menu-template').text());
     DevList.Templates.Edition = Hogan.compile($('#edition-template').text());
-    DevList.Views.MainMenu = new DevList.App._Views.MainMenu({ model: DevList.Models.Years, el: '#main-menu' });
-    DevList.Views.Edition = new DevList.App._Views.Edition({ model: DevList.Models.CurrentEdition, el: '#edition' });
 
+    DevList.Router = new DevList.App._Router();
+
+    DevList.Models.Years = new DevList.App._Models.Years();
+    DevList.Views.MainMenu = new DevList.App._Views.MainMenu({ model: DevList.Models.Years, el: '#main-menu' });
     DevList.Models.Years.fetch();
-   // DevList.Models.CurrentEdition.fetch();
-    //$('#edition').html(DevList.Templates.Edition.render());
 });
